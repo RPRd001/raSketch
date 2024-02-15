cf4541adf859e66728f57ba44bf8449c2507e4b5
Ra <132078939+RPRd001@users.noreply.github.com>
2024-02-15 12:50:25 -0500

    Added the To Do instructions for improving raSketch

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
+// Initializing the sketch container
 const sketchContainer = document.querySelector('.sketch-container')
 
+// Displaying a blank sketch
 sketchContainer.innerHTML = ""
+
+/*
+    To Do:
+    1. Add a button + function for the user to input
+        the size of the sketch.
+    2. Modify paint() and make a click-&-hold eventlistener
+        for painting the pixels.
+    3. Add the functions to the 'Select Color' and 'Erase'
+        buttons
+*/
 
 function createPixels (size) {
     for (let i = 1; i <= (2 * size); i++) {
@@ -11,12 +23,16 @@ function createPixels (size) {
 
 function paint() {
     const pixels = document.querySelectorAll('.pixel')
+    
     pixels.forEach(pixel => {
-        pixel.addEventListener('mouseout', () => {
+        // On each click will paint the div below the mouse
+        pixel.addEventListener('click', () => {
             pixel.style.backgroundColor = "white";
         })
+
     });
+    
 }
 
-// MAX = 50 - Otherwise is too much resources consumption (I guess...?)
-createPixels(50)
+// MAX = 100 - Otherwise is too much resources consumption (I guess...?)
+createPixels(200)
